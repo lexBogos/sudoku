b05e1cffeafcde75b7842382bbca8e1ada6b5dba
lexBogos <lex.bogos@gmail.com>
2019-03-20 11:33:08 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (matrix[row][col] === 0) {
+        for (let suspect = 1; suspect < 10; suspect++) {
+          if (appropriate_number(row, col, suspect, matrix)) {
+            if (solveSudoku(matrix)) {
+              return matrix;
+            }
+            matrix[row][col] = 0;
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;   
 }
+function appropriate_number( row, col, suspect, matrix,) {
+  for(let i = 0; i<9; i++){
+      if (matrix[row][i]===suspect){return false}
+      if (matrix[i][col]===suspect){return false}  
+      if(matrix[Math.floor(row/3)*3+Math.floor(i/3)][Math.floor(col/3)*3+i%3]===suspect){return false} 
+    }
+   return matrix[row][col] = suspect;
+}
+
+
+
+
+
+
+
+
+
+
